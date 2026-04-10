ff91a922554e096dad9c4fbe8608d5193eccd3b6
Abhivarun <chikotiabhivarun05@gmail.com>
2026-04-10 21:29:56 +0530

    Routing

--- index.js
+++ index.js
@@ -8,11 +8,24 @@ app.listen(port , ()=>{
     console.log(`app is listening on port ${port}`);
 })
 
-app.use((req,res)=>{
-    // console.log(req);
-    console.log("request recieved");
-    res.send({
-        name:"apple",
-        color:"red",
-    });
+// app.use((req,res)=>{
+//     // console.log(req);
+//     console.log("request recieved");
+//     res.send({
+//         name:"apple",
+//         color:"red",
+//     });
+// });
+
+app.get("/",(req,res)=>{
+    res.send("you contacted root path");
+});
+
+app.get("/apple",(req,res)=>{
+    res.send("you contacted apple path");
+});
+
+
+app.post("/",(req,res)=>{
+    res.send("you sent a post request");
 });
